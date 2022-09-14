0d54e1cfe5a5448515f60e15a82e25b1b941d060
NoelAlmighty <noelisedu@gmail.com>
2022-09-14 06:18:03 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabetc.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * Description - prints a-z in caps, then small
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	char c = 'A';
+
+	while (c <='Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar ('\n');
+	Return(0);
+}
