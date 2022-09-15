40ce7872564efa9789801f2fb060079392cb6d78
NoelAlmighty <noelisedu@gmail.com>
2022-09-15 02:28:29 -0700

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,26 +18,26 @@ int main(void)
 		d = 0;
 		while (d < 10)
 		{
-			c = 0;
-				while (c < 10)
+		c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
 				{
-					if (c != d && d != e && e < d && d < c)
-					{
-						putchar('0' + e);
-						putchar('0' + d);
-						putchar('0' + c);
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
 
-						if (c + d + e != 9 + 8 + 7)
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
-					c++;
+					if (c + d + e != 9 + 8 + 7)
+					{
+					putchar(',');
+					putchar(' ');
 				}
-			d++;
-		}
-		e++;
+			}
+				c++;
+			}
+		d++;
+	}
+	e++;
 	}
 	putchar ('\n');
 	return (0);
