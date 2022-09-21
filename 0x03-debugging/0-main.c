e6d06556c32f6158603698cad7ec6d640fc414bc
NoelAlmighty <noelisedu@gmail.com>
2022-09-21 16:42:08 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
- * Description - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
